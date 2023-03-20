2514ab69b0737ce28eeb973d0892e0452c4eb464
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-03-20 23:43:33 +0200

    add answer task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+//^ Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+//? Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+//* Add markup for gallery
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+const makeGalleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+    <img class="gallery__image"
+    src=${preview} 
+    alt="${description}" />
+    </a>`
+})
+    .join('');
+    
+galleryEl.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+    
+//* Add SimpleLightbox
+    new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
 
 console.log(galleryItems);
