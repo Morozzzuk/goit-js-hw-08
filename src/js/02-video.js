e3481060743f7680aaea1b27216dc6765a0f7e72
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-03-23 22:43:18 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,7 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const savedTime = localStorage.getItem(CURRENT_TIME);
 
 function onPlay(data) {
-  // data is an object containing properties specific to that event
+  //^ data is an object containing properties specific to that event
   localStorage.setItem(CURRENT_TIME, data.seconds);
 }
 
