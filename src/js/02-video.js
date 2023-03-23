a17f490bd2763e03ca42a47a6abe12e5197855f9
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-03-23 23:05:56 +0200

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const CURRENT_TIME = 'videoplayer-current-time';
-const savedTime = localStorage.getItem(CURRENT_TIME);
+const TIME_STORAGE = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(TIME_STORAGE);
 
 function onPlay(data) {
   //^ data is an object containing properties specific to that event
-  localStorage.setItem(CURRENT_TIME, data.seconds);
+  localStorage.setItem(TIME_STORAGE, data.seconds);
 }
 
 player.on('timeupdate', throttle(onPlay, 1000));
